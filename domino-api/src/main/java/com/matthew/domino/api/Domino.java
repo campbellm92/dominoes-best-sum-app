083b08d598c269ca07d447da7f030afb0d150fee
Matthew Campbell <m.campbell92@gmail.com>
2025-11-22 09:10:06 +0100

    make representation class for dominoes

---
+++ domino-api/src/main/java/com/matthew/domino/api/Domino.java
@@ -0,0 +1,24 @@
+package com.matthew.domino.api;
+
+// representation class / POJO -> map java to JSON
+
+public class Domino {
+    private int left;
+    private int right;
+
+    public int getLeft() {
+        return left;
+    }
+
+    public void setLeft(int left) {
+        this.left = left;
+    }
+
+    public int getRight() {
+        return right;
+    }
+
+    public void setRight(int right) {
+        this.right = right;
+    }
+}
