d2b6e9d56591e3973fc1031688060b28c3074238
Matthew Campbell <m.campbell92@gmail.com>
2025-11-22 09:57:01 +0100

    add validation, change int primitives to Integer classes

--- domino-api/src/main/java/com/matthew/domino/api/Domino.java
+++ domino-api/src/main/java/com/matthew/domino/api/Domino.java
@@ -1,24 +1,42 @@
 package com.matthew.domino.api;
 
-// representation class / POJO -> map java to JSON
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+// representation class / POJO -> map Java object to JSON
 
 public class Domino {
-    private int left;
-    private int right;
 
-    public int getLeft() {
+    @NotNull(message = "Left value cannot be empty.")
+    @Min(value = 1, message = "Value must be >= 1.")
+    @Max(value = 10, message = "Value must be <= 10.")
+    private Integer left;
+
+    @NotNull(message = "Right value cannot be empty.")
+    @Min(value = 1, message = "Value must be >= 1.")
+    @Max(value = 10, message = "Value must be <= 10.")
+    private Integer right;
+
+    @AssertTrue(message = "Left and right values cannot be equal.")
+    public boolean isValidTile() {
+        return left != right;
+    }
+
+    public Integer getLeft() {
         return left;
     }
 
-    public void setLeft(int left) {
+    public void setLeft(Integer left) {
         this.left = left;
     }
 
-    public int getRight() {
+    public Integer getRight() {
         return right;
     }
 
-    public void setRight(int right) {
+    public void setRight(Integer right) {
         this.right = right;
     }
 }
