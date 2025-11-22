f88d83b3e0038b0b9bfba2aefca0e01029fb5268
Matthew Campbell <m.campbell92@gmail.com>
2025-11-22 09:57:30 +0100

    add validation

--- domino-api/src/main/java/com/matthew/domino/resources/BestChainRequest.java
+++ domino-api/src/main/java/com/matthew/domino/resources/BestChainRequest.java
@@ -1,10 +1,15 @@
 package com.matthew.domino.resources;
 
 import java.util.List;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 
 import com.matthew.domino.api.Domino;
 
 public class BestChainRequest {
+
+    @NotEmpty(message = "Tiles must not be empty.")
+    @Valid
     private List<Domino> tiles;
 
     public List<Domino> getTiles() {
